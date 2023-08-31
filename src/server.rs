0e1c98b188397a9585c6e7fd9cfdba5361e53d6c
nanoqsh <nanoqsh@gmail.com>
2023-08-31 09:44:02 +0600

    Redirect

--- src/server.rs
+++ src/server.rs
@@ -1,10 +1,17 @@
 use {
     crate::config::Net,
-    axum::{response::Html, routing::get, Router, Server},
+    axum::{
+        extract::Path,
+        http::StatusCode,
+        response::{Redirect, Result},
+        routing::get,
+        Router, Server,
+    },
+    std::collections::HashMap,
 };
 
 pub async fn run(conf: Net) {
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new().route("/:key", get(lookup));
     let addr = conf.socket_addr();
     println!("listening on http://{addr}");
 
@@ -14,6 +21,8 @@ pub async fn run(conf: Net) {
     }
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+async fn lookup(Path(path): Path<String>) -> Result<Redirect> {
+    let map = HashMap::from([(String::from("lol"), String::from("https://lol.com"))]);
+    let link = map.get(&path).ok_or(StatusCode::NOT_FOUND)?;
+    Ok(Redirect::temporary(link))
 }
